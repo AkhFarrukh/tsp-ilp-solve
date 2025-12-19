e52847f15f25e0f174342ea26145ea7a8159ec47
Farrukh <akhmedfarrukh@gmail.com>
2025-12-19 14:06:37 +0100

    Remove cycle prints ofr continious solutions as written in Enonce pdf

--- tsp_solver.py
+++ tsp_solver.py
@@ -24,13 +24,13 @@ def main():
         print(f"Cycle: {get_tour_string(n, x)}")
     elif method_flag == 1:
         val, x, time_taken = solve_mtz(n, dist_matrix, relax=True)
-        print(f"Cycle: {get_tour_string(n, x)}")
+        #print(f"Cycle: {get_tour_string(n, x)}")
     elif method_flag == 2:
         val, x, time_taken = solve_dfj_enum(n, dist_matrix, relax=False)
         print(f"Cycle: {get_tour_string(n, x)}")
     elif method_flag == 3:
         val, x, time_taken = solve_dfj_enum(n, dist_matrix, relax=True)
-        print(f"Cycle: {get_tour_string(n, x)}")
+        #print(f"Cycle: {get_tour_string(n, x)}")
     elif method_flag == 4:
         val, x, time_taken, iterations = solve_dfj_iter(n, dist_matrix)
         print(f"Cycle: {get_tour_string(n, x)}")
